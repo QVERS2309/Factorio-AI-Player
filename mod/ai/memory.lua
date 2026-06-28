7467e289c3b5942366f5fd48d793238eb6bf17f1
QVERS2309 <qvers2309@gmail.com>
2026-06-28 22:58:34 +0500

    v0.0.3 - World awareness

--- mod/ai/memory.lua
+++ mod/ai/memory.lua
@@ -3,12 +3,31 @@ local Logger = require("util.logger")
 local Memory = {}
 
 function Memory.init()
+
     storage.ai = storage.ai or {
-        version = "0.0.2",
-        state = "idle"
+
+        version = "0.0.3",
+
+        state = "idle",
+
+        player = {
+            position = nil
+        },
+
+        target = {
+            entity = nil,
+            position = nil,
+            type = nil
+        },
+
+        world = {
+            iron = nil
+        }
+
     }
 
     Logger.info("Memory initialized")
+
 end
 
 return Memory

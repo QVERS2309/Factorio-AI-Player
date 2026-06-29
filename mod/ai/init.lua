e37dd6301f9b5db08ddf44c3b41caf0ea622b964
QVERS2309 <qvers2309@gmail.com>
2026-06-29 10:23:14 +0500

    v0.3.1 - Continuous navigation update

--- mod/ai/init.lua
+++ mod/ai/init.lua
@@ -1,11 +1,16 @@
 local Brain = require("ai.brain")
 local Memory = require("ai.memory")
+local Navigation = require("ai.navigation")
 
 require("ai.player")
 require("ai.world")
 require("ai.knowledge")
 
 local AI = {}
+
+----------------------------------------------------
+-- Инициализация
+----------------------------------------------------
 
 function AI.init()
 
@@ -14,7 +19,13 @@ function AI.init()
 
 end
 
+----------------------------------------------------
+-- Обновление
+----------------------------------------------------
+
 function AI.update(tick)
+
+    Navigation.update()
 
     Brain.update(tick)
 

0549b696944fafa1525701da6035d68bc604b1d0
QVERS2309 <qvers2309@gmail.com>
2026-06-29 10:42:20 +0500

    v0.3.3 - AI runtime controller

--- mod/control.lua
+++ mod/control.lua
@@ -1,8 +1,27 @@
 local AI = require("ai.init")
+local Controller = require("ai.controller")
 
 script.on_init(function()
     AI.init()
 end)
+
+commands.add_command(
+    "ai",
+    "Toggle AI",
+    function()
+
+        Controller.toggle()
+
+        local state = "DISABLED"
+
+        if Controller.is_enabled() then
+            state = "ENABLED"
+        end
+
+        game.print("[AI] " .. state)
+
+    end
+)
 
 script.on_event(defines.events.on_tick, function(event)
     AI.update(event.tick)
